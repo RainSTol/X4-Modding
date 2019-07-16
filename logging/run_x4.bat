330ae14d288761ac662880ec80f451937fbf0eed
RainSTol <52976634+RainSTol@users.noreply.github.com>
2019-07-16 12:44:13 -0700

    Create run_x4.bat

---
+++ logging/run_x4.bat
@@ -0,0 +1,4 @@
+X4.exe -showfps -logfile debuglog.txt -scriptlogfiles -debug all
+REM -skipintro -instantplayerrespawn
+move /y %UserProfile%\Documents\Egosoft\X4\****your steam user id***\debuglog.txt %UserProfile%\Documents\Egosoft\X4\****your steam user id***\debuglog-old.txt
+REM pause
